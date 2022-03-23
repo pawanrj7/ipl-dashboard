a877a2fb24ec04e6d2a730a4b211b6182f2bc894
pawanrj7 <pr.pawanrj7@gmail.com>
2022-03-23 15:03:26 +0530

    output logs refactored

--- src/main/java/io/rao/ipldashboard/Model/Team.java
+++ src/main/java/io/rao/ipldashboard/Model/Team.java
@@ -1,9 +1,12 @@
 package io.rao.ipldashboard.Model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Team {
@@ -14,6 +17,11 @@ public class Team {
     private String teamName;
     private long totalMatches;
     private long totalWins;
+    
+    @Transient
+    private List<Match> matches;
+
+
     public long getId() {
         return id;
     }
@@ -38,11 +46,22 @@ public class Team {
     public void setTotalWins(long totalWins) {
         this.totalWins = totalWins;
     }
-
-
     public Team(String teamName, long totalMatches) {
         this.teamName = teamName;
         this.totalMatches = totalMatches;
     }
-    
+    @Override
+    public String toString() {
+        return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
+    }
+    public Team() {
+        
+    }
+    public List<Match> getMatches() {
+        return matches;
+    }
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
+    }
+  
 }
